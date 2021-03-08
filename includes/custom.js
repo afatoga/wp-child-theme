6b1f7980029728aeff1cd9dbd3586d383ff724a8
afatoga <afatoga@gmail.com>
2021-03-08 01:04:55 +0100

    recaptcha

--- includes/custom.js
+++ includes/custom.js
@@ -1,24 +1,43 @@
-function af_validateRequestForm(event) {
-  event.preventDefault()
-  //const form = document.querySelector("form[name='registration']")
-  const data = new FormData(event.target)
-    
+function af_regForm_onSubmit(event) {
+
+  const form = document.querySelector("form[name='registration']")
+  if (!form.checkValidity()) return;
+
+  const data = new FormData(form)
+
   if (!data.get("email").length) return false
 
-	fetch(wpRestApi.root + "/submit_registration_form", {
-		method: 'POST',
-		body: new FormData(event.target),
-        headers: {
-            'Accept': 'application/json'
-        },
-	}).then(function (response) {
-		if (response.ok) {
-			return response.json();
-		}
-		return Promise.reject(response);
-	}).then(function (data) {
-		console.log(data);
-	}).catch(function (error) {
-		console.warn(error);
-	});
+  event.preventDefault()
+
+  grecaptcha.ready(function () {
+    grecaptcha
+      .execute(wpRestApi.recaptchaSiteKey, { action: "submit" })
+      .then(function (token) {
+        data.append("g-recaptcha-response", token)
+
+        af_sendRequest("/submit_registration_form", data)
+      })
+  })
+}
+
+function af_sendRequest(apiRoute, data) {
+  fetch(wpRestApi.root + apiRoute, {
+    method: "POST",
+    body: data,
+    headers: {
+      Accept: "application/json",
+    },
+  })
+    .then(function (response) {
+      if (response.ok) {
+        return response.json()
+      }
+      return Promise.reject(response)
+    })
+    .then(function (data) {
+      console.log(data)
+    })
+    .catch(function (error) {
+      console.warn(error)
+    })
 }
--- reg_request_page.php
+++ reg_request_page.php
@@ -1,6 +1,6 @@
 <?php /* Template Name: reg_request */
 
-
+$recaptchaSiteKey = "6LegGHYaAAAAAAvyBXcVrLC5GRlcWLxCVhKcI0Pq";
 $isIE =
     preg_match('~MSIE|Internet Explorer~i', $_SERVER['HTTP_USER_AGENT']) ||
     preg_match('~Trident/7.0(.*)?; rv:11.0~', $_SERVER['HTTP_USER_AGENT']);
@@ -9,18 +9,23 @@ if ($isIE) {
     wp_enqueue_script('unfetch-polyfill', 'https://unpkg.com/unfetch@4.2.0/polyfill/index.js');
 }
 
+wp_enqueue_script('google-recaptcha', 'https://www.google.com/recaptcha/api.js?render='.$recaptchaSiteKey, [], false, true);
 wp_enqueue_script('af-custom', get_stylesheet_directory_uri().'/includes/custom.js', [], false, true);
 wp_localize_script( 'af-custom', 'wpRestApi', [
-    'root'  => esc_url_raw( rest_url().'aa_restserver/v1' )
+    'root'  => esc_url_raw( rest_url().'aa_restserver/v1' ),
+    'recaptchaSiteKey' => $recaptchaSiteKey
 ] );
 
 get_header(); ?>
 
-<form name="registration" method="POST" onsubmit="return af_validateRequestForm(event);">
+<form name="registration" method="POST">
 
 <label for="email">Email</label>
 <input type="email" name="email" id="email">
-<button type="submit">Odeslat žádost</button>
+<button 
+        type="button"
+        onclick='af_regForm_onSubmit(event)'
+        >Odeslat žádost</button>
 </form>
 
 <?php get_footer(); ?>
